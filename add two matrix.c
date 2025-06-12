4386d7dca568955e01a00d08282586065d68f236
sujalkide <sujalkide@gmail.com>
2025-06-12 16:39:11 +0530

    Create add two matrix.c

---
+++ add two matrix.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ()
+{
+  int a[3][3]={{1,1,1},{1,1,1},{1,1,1}};
+  int b[3][3]={{1,1,1},{1,1,1},{1,1,1}};
+  int c[3][3];
+  int i,j;
+for(i=0; i<3; i++)
+{
+  for(j=0; j<3; j++)
+{
+  c[i][j]=a[i][j]+b[i][j];
+  printf("%d",c[i][j]);
+}
+ printf("\n");
+}
+return 0;
+}
