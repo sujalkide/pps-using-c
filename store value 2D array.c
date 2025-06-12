383b565bde8cce9858320867d1b597a949794d14
sujalkide <sujalkide@gmail.com>
2025-06-12 16:52:31 +0530

    Create store value 2D array.c

---
+++ store value 2D array.c
@@ -0,0 +1,18 @@
+include<stdio.h>Add commentMore actions
+int main ()
+{
+int i,j,a[3][3]={{1,2,3},{6,7,8},{5,4,9}};
+int i,j;
+for(i=0; i<3; i++)
+{
+  for(j=0; j<3; i++)
+{
+  printf("Enter value row=%d,colum=%d,i,j);
+  scanf("%d";a[i][j]);
+}
+ printf("\n");
+}
+ return 0;
+}
+
+ 
